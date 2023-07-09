0342dcfc00e51b09f971ec50b9d68ac32d922fdf
GANESH <ganesh2638@gmail.com>
2023-07-09 21:26:19 +0530

    Demo class added

--- src/main/java/practise/Demo.java
+++ src/main/java/practise/Demo.java
@@ -1,11 +1,12 @@
 package practise;
 
-public class Test {
+public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("Test");
-      System.out.println("Hello");
+
+		System.out.println("Hello");
+		System.out.println("New Branch--- JAVA");
 	}
 
 }
