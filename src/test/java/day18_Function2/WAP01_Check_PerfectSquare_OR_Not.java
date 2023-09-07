199f819f00883f1348fca071d462ef7078756a0f
ganesh2638 <=>
2023-09-07 20:47:14 +0530

    Added day16 session program

---
+++ src/test/java/day18_Function2/WAP01_Check_PerfectSquare_OR_Not.java
@@ -0,0 +1,33 @@
+package day18_Function2;
+
+import java.util.Scanner;
+
+/**
+ * Given n, return 1 if n is perfect square otherwise return 0 
+ */
+
+public class WAP01_Check_PerfectSquare_OR_Not {
+	
+	public static int checkPerfectSquare(int n) {
+		
+		int ans = 0;
+		for(int i=1; i*i<=n; i++) {
+			if(i*i==n) {
+				return 1;
+			}
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter the number:");
+		int n = sc.nextInt();
+		
+		int n1 = checkPerfectSquare(n);
+		System.out.println(n1);
+		
+
+	}
+
+}
