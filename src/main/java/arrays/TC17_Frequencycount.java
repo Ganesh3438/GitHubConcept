9fbb29cd87f388c581eac75097dbda1ca79942b6
ganesh2638 <=>
2023-09-25 07:47:07 +0530

    Added frequency count problem

--- src/main/java/arrays/TC17_Frequencycount.java
+++ src/main/java/arrays/TC17_Frequencycount.java
@@ -7,16 +7,19 @@ public class TC17_Frequencycount {
 		int n = arr.length;
 		int[] newArray = new int[n];
 		// {1, 2, 5, 2, 4, 1}
-		int count = 0;
+		
 		
 		for(int i=0; i<n; i++) {
+			int count = 0;
 			int val = arr[i];
 			for(int j=0; j<n; j++) {
 				if(val == arr[j]) {
-					newArray[count] = val;
 					count++;
+					
 				}
 			}
+			
+			newArray[i] = count;
 		}
 		
 		return newArray;
@@ -25,6 +28,14 @@ public class TC17_Frequencycount {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int[] arr = {1, 2, 3, 2, 4, 1, 6};
+		int[] status = solve(arr);
+		
+		for(int i=0; i<status.length; i++) {
+		System.out.print(status[i] + " ");
+		}
+		 
+		
 	}
 
 }
